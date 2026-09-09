e46ab22105187ca323f3e7ecf71287381f9b93f2
seanredenbaugh <seanredenbaugh@yahoo.com>
2026-09-09 18:10:33 -0500

    Add database configuration to config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+'database' => [
+    'host' => 'localhost',
+    'port' => 3306,
+    'name' => 'FULL_HOSTINGER_DATABASE_NAME',
+    'username' => 'FULL_HOSTINGER_DATABASE_USERNAME',
+    'password' => 'YOUR_DATABASE_PASSWORD',
+    'charset' => 'utf8mb4',
+],
